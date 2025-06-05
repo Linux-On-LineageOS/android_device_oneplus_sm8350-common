0bbfb64cd9db63d42a636e6dfd01d11b881d45ed
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:37:46 +0800

    Disable SELinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -208,3 +208,4 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/sm8350-common/BoardConfigVendor.mk
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
